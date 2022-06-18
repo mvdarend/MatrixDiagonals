950bc5143d14f04667590c064260f42879e4024c
Marcel van den Arend <marcel@vandenarend.nl>
2022-06-18 10:18:44 +0200

    test: Added test project

---
+++ MatrixTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MatrixTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
